2c2d92d9a24cbbe69682f6d9df5c029a12b3af6a
tako1995 <141060940+tako1995@users.noreply.github.com>
2023-09-23 03:05:20 +0400

    edit

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-let burgBar =document.querySelector(".fa-bars");
-let list =document.querySelector(".main_header_bar");
-let xMark =document.querySelector('.fa-xmark');
+// let burgBar =document.querySelector(".fa-bars");
+// let list =document.querySelector(".main_header_bar");
+// let xMark =document.querySelector('.fa-xmark');
 
-burgBar.addEventListener("click", ()=>{
-list.style.display = "block"
-burgBar.style.display = "none"
-xMark.style.display = "block"
+// burgBar.addEventListener("click", ()=>{
+// list.style.display = "block"
+// burgBar.style.display = "none"
+// xMark.style.display = "block"
 
-});
- xMark.addEventListener('click',()=>{
-    list.style.display = "none"
-    burgBar.style.display = "block"
-    xMark.style.display = "none"
- })
+// });
+//  xMark.addEventListener('click',()=>{
+//     list.style.display = "none"
+//     burgBar.style.display = "block"
+//     xMark.style.display = "none"
+//  })
+
+
+
